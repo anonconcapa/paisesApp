afaaf7d749e514423443c4dbb338a0b198678140
mkkbar <joelluna91827@gmail.com>
2023-01-04 17:46:32 -0300

    aplicacion de paises

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html'
+})
+export class PorCapitalComponent  {
+
+  placeholderCapital:string = 'buscar capital';
+
+  termino:string = '';
+  hayError:boolean = false;
+  paises:Country[] =[];
+
+  constructor(private paisService:PaisService) { }
+
+  buscar(termino:string){
+    this.hayError = false;
+
+    this.termino = termino;
+
+
+    this.paisService.buscarCapital(termino).subscribe((capital) =>{
+      console.log(capital);
+      this.paises = capital;
+    },(err)=>{
+      this.hayError = true;
+    })
+
+  }
+
+  sugerencias(){
+    this.hayError = false;
+  }
+
+
+}
---
+++ src/app/pais/pais.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { RouterModule } from '@angular/router';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    VerPaisComponent,
+    PorRegionComponent,
+    TablaComponent,
+    PaisInputComponent
+  ],
+  exports:[
+    PorCapitalComponent,
+    PorPaisComponent,
+    PorRegionComponent,
+    VerPaisComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
+})
+export class PaisModule { }
